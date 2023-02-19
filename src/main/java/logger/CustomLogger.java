bb6df5d26924671b26cbd69953463947147d4e7e
TonyMontana <pr.grigoriev.anton@gmail.com>
2023-02-19 15:57:36 +0300

    1
    test

--- src/main/java/logger/CustomLogger.java
+++
@@ -1,9 +0,0 @@
-package logger;
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class CustomLogger {
-    public static Logger logger = LogManager.getLogger(CustomLogger.class);
-}
